f32ffe649f9c717dd4a18440337923f938e5972a
UncleH25 <oneilljarryd@gmail.com>
2025-05-01 20:27:15 +0200

    Added code to prompt the user for input, take in the put, and assigned them to their respective variable. Also added an input bufferer.

--- Library Checkout System.cpp
+++ Library Checkout System.cpp
@@ -15,5 +15,21 @@ int main()
 	int daysDue;
 	char isTextbook;
 
+	//Input
+	cout << "Enter your name: \n";
+	getline(cin, userName);
+
+	cout << "Enter the title of the book: \n";
+	getline(cin, bookTitle);
+
+	cout << "How many days is the book due in? \n";
+	cin >> daysDue;
+
+	// Clear the newline character from the input buffer
+	cin.ignore();
+
+	cout << "Is the book a textbook? (Y/N): \n";
+	cin >> isTextbook;
+
 	return 0;
 }
