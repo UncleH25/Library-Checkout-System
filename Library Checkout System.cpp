55e21dd96a4630f3b1cca1a29975a1909617d515
UncleH25 <oneilljarryd@gmail.com>
2025-05-01 20:26:02 +0200

    Declared initial variables.

--- Library Checkout System.cpp
+++ Library Checkout System.cpp
@@ -6,6 +6,14 @@ using namespace std;
 
 int main()
 {
-	
+	//Constants
+	const double NORMAL_RATE = 1.0;
+	const double TEXTBOOK_RATE = 2.0;
+
+	//Variables
+	string userName, bookTitle;
+	int daysDue;
+	char isTextbook;
+
 	return 0;
 }
